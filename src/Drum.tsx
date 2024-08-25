f539d923105e0108fae70d99d50316e9d605c316
unknown <tsmith110179@yahoo.com>
2024-08-24 20:34:30 -0500

    another one

--- src/Drum.tsx
+++ src/Drum.tsx
@@ -8,7 +8,7 @@ const Drum = ({audioClip}: DrumProps) => {
     const playSound = (clip: AudioClip) => {
         document.getElementById(clip.keyTrigger as HTMLAudioElement).play()
         .catch(console.error);
-        document.getElementById('display')!.innerText = clip.description
+        document.getElementById('display')!.innerText = clip.description;
     }
     return (
        <button className="drum-pad" id={`drum-${audioClip.keyTrigger}`} onClick={() => playSound(audioClip)}>
